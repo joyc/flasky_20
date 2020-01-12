80e076d64aa4df335d568a3603310b6c094054f8
joyc <joyc@live.jp>
2020-01-12 21:00:02 +0900

    use Flask-Bootstrap for templates

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -18,6 +20,13 @@ app.add_url_rule('/top', 'top', top)
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('404.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
