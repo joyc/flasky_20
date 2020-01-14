1c7662ce244d2220337f22e3ee91771a7006bc9d
joyc <joyc@live.jp>
2020-01-14 20:54:33 +0900

    add flask-mail for mail notification

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -26,6 +27,24 @@ migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky20]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky20 Admin <PyBee0@gmail.com>'
+app.config['FLASKY20_ADMIN'] = os.environ.get('FLASKY20_ADMIN')
+mail = Mail(app)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, \
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 @app.shell_context_processor
 def make_shell_context():
@@ -64,15 +83,16 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            db.session.commit()
             session['known'] = False
+            if app.config['FLASKY20_ADMIN']:
+                send_email(app.config['FLASKY20_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
-            session['knows'] = True
+            session['known'] = True
         session['name'] = form.name.data
-        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
-        known=session.get('known', False), current_time=datetime.utcnow())
+                           known=session.get('known', False), current_time=datetime.utcnow())
 
 def top():
     return "<h2>Ni hao Flask世界！<br> 我是%s</h2>" % "言言"
