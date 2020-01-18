5389fb7bafb502ff306bd3d5d9a25ef34ef19082
joyc <joyc@live.jp>
2020-01-18 17:20:40 +0900

    add auth user logout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,3 +3,13 @@
 # @File   : forms.py
 # @Author : Hython
 # @Date   : 公元 2020/01/18 12:33
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
--- app/main/views.py
+++ app/main/views.py
@@ -15,22 +15,7 @@ from flask_login import login_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if app.config['FLASKY20_ADMIN']:
-                send_email(app.config['FLASKY20_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False), current_time=datetime.utcnow())
+    return render_template('index.html')
 
 
 @main.route('/secret')
