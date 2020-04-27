488c8dee2f51cc4e1fccaeff69df59af23e3bd88
joyc <joyc@live.jp>
2020-04-27 23:57:20 +0900

    add user page

--- app/main/views.py
+++ app/main/views.py
@@ -22,3 +22,9 @@ def index():
 @login_required
 def secret():
     return 'Only authenticated users are allowed!'
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
