eb20abcba1373a9dc100c9b94ff0ed3dd5c740df
joyc <joyc@live.jp>
2020-05-06 23:58:50 +0900

    add profile edit form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,10 +4,18 @@
 # @Author : Hython
 # @Date   : 公元 2020/01/14 23:36
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    """user profile edit form"""
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
