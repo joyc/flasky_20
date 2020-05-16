53fba3023fabe41aaaa556de2a743721e4aca5e7
joyc <joyc@live.jp>
2020-05-16 23:36:14 +0900

    add posts module

--- app/models.py
+++ app/models.py
@@ -89,6 +89,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
